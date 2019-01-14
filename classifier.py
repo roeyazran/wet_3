b74b89af958c04b12276f86c96a9132a5516dad6
roeyazran <roeyazran@campus.technion.ac.il>
2019-01-14 17:33:35 +0200

    branch 5

--- classifier.py
+++ classifier.py
@@ -1,8 +1,9 @@
 data_path = r'data/Data.pickle'
 import pickle
 import numpy as np
-import sklearn
+from sklearn import *
 from hw3_utils import *
+from sklearn.linear_model import Perceptron
 import random
 import csv
 
@@ -10,8 +11,9 @@ GlobalDataSet = load_data(data_path);
 
 def euclidean_distance(ObjectA, ObjectB):
     distance = 0
-    for (featureA1,featureA2) in (ObjectA, ObjectB):
-        distance += (featureA1-featureA2)**2
+    #print ("euclidean dist, lists lens:",len(ObjectA), ", " , len(ObjectB))
+    for index in range(len(ObjectA)):
+        distance += (ObjectA[index]-ObjectB[index])**2
     return distance**(0.5)
 
 
@@ -25,12 +27,15 @@ class knn_classifier(abstract_classifier):
     def classify(self, features):
         ObjectsDistances = np.empty(len(self.DataFeatures))
         ObjectsLabels = np.empty(len(self.DataLabels))
-        for index, (object, label) in enumrate((self.DataFeatures,self.DataLabels)):
-            ObjectsDistances[index]=euclidean_distance(object, features)
-            ObjectsLabels[index]=label
+        for index in range(len(self.DataFeatures)):
+            ObjectsDistances[index] = euclidean_distance(self.DataFeatures[index], features)
+            #print ("distance: ", ObjectsDistances[index])
+            ObjectsLabels[index] = self.DataLabels[index]
         countPos = 0
         countNeg = 0
+        #counting the classification of the closest k items
         for index in ObjectsDistances.argsort()[:self.k]:
+            #print (index," closest distance: ", ObjectsDistances[index])
             if ObjectsLabels[index]:
                 countPos += 1
             else:
@@ -43,6 +48,7 @@ class knn_factory(abstract_classifier_factory):
         self.k = k
 
     def train(self, data, labels):
+        #print("data:", len(data), " labels: ", len(labels))
         return knn_classifier(data,labels, self.k)
 
 
@@ -80,7 +86,7 @@ def split_crosscheck_groups (dataset, num_folds):
             ElementToAdd = random.choice(NegList)
             ECGFoldLists[index].append(ElementToAdd)
             ECGFoldLabels[index].append(False)
-            NegList = removeElementFromList(NegList,ElementToAdd)
+            NegList = removeElementFromList(NegList, ElementToAdd)
             if len(NegList) == 0:
                 break
 
@@ -94,32 +100,30 @@ def load_k_fold_data(index):
         train_index, labels_index = pickle.load(f)
     return train_index, labels_index
 
-if __name__ == '__main__':
-#    print(GlobalDataSet)
-#    split_crosscheck_groups(GlobalDataSet, 2)
-    load_k_fold_data(2)
-
 def evalute(classifier_factory, k):
     foldsLists = list()
     DatasetLen = 0
     for index in range(1, k+1):
         foldsLists.append(load_k_fold_data(index))
         DatasetLen += len(load_k_fold_data(index)[0])
-
     TruePos = 0
     TrueNeg = 0
     FalsePos = 0
     FalseNeg = 0
-    for index in range(1, k+1):
+    for index in range(k):
+        print ("Testing ", index + 1, " fold")
         TraningSetData = list()
         TraningSetLabels = list()
-        for j in range(1, k + 1):
+        for j in range(k):
             if j != index:
-                TraningSetLabels.append(foldsLists[j][1])
-                TraningSetData.append(foldsLists[j][0])
+                #print(foldsLists[j][0])
+                TraningSetLabels.extend(foldsLists[j][1])
+                TraningSetData.extend(foldsLists[j][0])
         Classifier = classifier_factory.train(TraningSetData, TraningSetLabels)
-        for sample,label in foldsLists[index][0],foldsLists[index][1]:
+        for SampleIndex, sample in enumerate(foldsLists[index][0]):
+            label = foldsLists[index][1][SampleIndex]
             ClassRes = Classifier.classify(sample)
+            #print (label)
             if label == True:
                 if ClassRes == True:
                     TruePos += 1
@@ -130,19 +134,58 @@ def evalute(classifier_factory, k):
                     TrueNeg += 1
                 else:
                     FalsePos += 1
-
-        AvgAccuracy = (TruePos+TrueNeg)/DatasetLen
-        AvgError = (FalsePos + FalseNeg)/DatasetLen
-        return AvgAccuracy, AvgError
+    AvgAccuracy = (TruePos+TrueNeg)/DatasetLen
+    AvgError = (FalsePos + FalseNeg)/DatasetLen
+    print("OverAll results folds= ", k, "AvgAccuracy= ", AvgAccuracy, "AvgError= ", AvgError )
+    return AvgAccuracy, AvgError
 
 
 def experiments6():
     with open('experiments6.csv', mode= 'w') as experiments6_file:
-        exp6_writer= csv.writer(experiments6_file, delimiter=',', qutechar='"', quoting = csv.QUOTE_MINIMAL)
+        exp6_writer= csv.writer(experiments6_file, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
         for k in [1, 3, 5, 7, 13]:
+            print("Testing for k = ",k)
             ClassifierFactory = knn_factory(k)
-            acc,err = evalute(ClassifierFactory, 2)
+            acc, err = evalute(ClassifierFactory, 2)
             exp6_writer.writerow([k, acc, err])
 
 
+class DIClassifier(tree.DecisionTreeClassifier):
+    def classify(self, features):
+        return self.predict([features])[0]
+
+class DI_factory(abstract_classifier_factory):
+    def train(self, data, labels):
+        #print("data:", len(data), " labels: ", len(labels))
+        Classifier = DIClassifier()
+        return Classifier.fit(data, labels)
+
+
+class PerceptronFactory(abstract_classifier_factory):
+    def train(self, data, labels):
+        #print("data:", len(data), " labels: ", len(labels))
+        Classifier = PerceptronClassifier()
+        return Classifier.fit(data, labels)
+
+
+class PerceptronClassifier(Perceptron):
+    def classify(self, features):
+        return self.predict([features])[0]
+
+
+def experiment1():
+    with open('experiments12.csv', mode= 'w') as experiments1_file:
+        exp1_writer= csv.writer(experiments1_file, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        ClassifierFactory = DI_factory()
+        acc, err = evalute(ClassifierFactory, 2)
+        exp1_writer.writerow(["1", acc, err])
+        ClassifierFactory= PerceptronFactory()
+        acc, err = evalute(ClassifierFactory, 2)
+        exp1_writer.writerow(["2", acc, err])
+
+if __name__ == '__main__':
+#    print(GlobalDataSet)
+#    split_crosscheck_groups(GlobalDataSet, 2)
+    experiments6()
+#    experiment1()
 
